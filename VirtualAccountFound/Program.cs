75b41a38f37c009872da1a245028f99f2c575967
gildarts <yaoming.huang@ischool.com.tw>
2014-02-07 10:57:37 +0800

    第一版本。

---
+++ VirtualAccountFound/Program.cs
@@ -0,0 +1,23 @@
+﻿using FISCA;
+using FISCA.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualAccountFound
+{
+    public static class Program
+    {
+        [MainMethod]
+        public static void Main()
+        {
+            RibbonBarItem ribbon = MotherForm.RibbonBarItems["總務作業", "對帳"];
+            ribbon["單筆查詢"].Click += delegate
+            {
+                new VAFinder().ShowDialog();
+            };
+        }
+    }
+}
